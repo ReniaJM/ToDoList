e78a4f5e8ae7f157b02a2b1b0cc124f031884a0a
ReniaJM <kontakt@jachtomadesign.pl>
2018-09-14 18:42:08 +0200

    test

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'ToDoList';
-  newTask: string;
   taskList: Array<string>=[];
   taskDone: Array<string>=[];
 
-  add(){
-    this.taskList.push(this.newTask);
-    this.newTask = ''
+  ngOnInit():void {
+    this.taskList = ['sprzatnie', 'jedzenie', 'spanie', 'kochanie']
+  }
+
+
+  add(task:string){
+    this.taskList.push(task);
 
   }
   remove(task:string){
